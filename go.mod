c0d643bebf2a5567a3efb290b21b6eb1e85a7eec
kekoyana <kekoyana0@gmail.com>
2021-02-19 00:44:10 +0900

    add template

--- go.mod
+++ go.mod
@@ -3,7 +3,7 @@ module getdel
 go 1.15
 
 require (
-	github.com/labstack/echo v3.3.10+incompatible // indirect
+	github.com/labstack/echo v3.3.10+incompatible
 	github.com/labstack/gommon v0.3.0 // indirect
 	github.com/mattn/go-colorable v0.1.8 // indirect
 	github.com/valyala/fasttemplate v1.2.1 // indirect
--- main.go
+++ main.go
@@ -1,15 +1,35 @@
 package main
 
 import (
+	"html/template"
+	"io"
 	"net/http"
 
 	"github.com/labstack/echo"
 )
 
+type Template struct {
+	templates *template.Template
+}
+
+func (t *Template) Render(w io.Writer, name string, data interface{}, c echo.Context) error {
+	return t.templates.ExecuteTemplate(w, name, data)
+}
+
 func main() {
+	t := &Template{
+		templates: template.Must(template.ParseGlob("views/*.html")),
+	}
+
 	e := echo.New()
+	e.Renderer = t
 	e.GET("/", func(c echo.Context) error {
-		return c.String(http.StatusOK, "Hello, World!")
+		data := struct {
+			Message string
+		}{
+			Message: "にゃーん",
+		}
+		return c.Render(http.StatusOK, "hello", data)
 	})
 	e.Logger.Fatal(e.Start(":1323"))
 }
